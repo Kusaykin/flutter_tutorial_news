b9522fbfcfc7551748acee5c67f1a0747009f2b6
dboyev <dboyev@nurbank.kz>
2018-10-09 17:35:09 +0600

    repository done

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,5 +1,16 @@
 import 'package:flutter/material.dart';
 
-class App {
+class App extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+  //   return MaterialApp(
+  //       home: Scaffold(
+  //           body: Text('data'),
+  //           floatingActionButton: FloatingActionButton(
+  //             child: Icon(Icons.add),
+  //           ),
+  //           appBar: AppBar(title: Text('Lets see some images!'))));
+  }  
 
 }
