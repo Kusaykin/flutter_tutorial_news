4d8b005d64855db3da7b5199c4c88c9ef52cdbae
dboyev <dboyev@nurbank.kz>
2018-12-24 19:34:35 +0600

    add .gitignore

---
+++ lib/src/blocs/stories_bloc.dart
@@ -0,0 +1,32 @@
+import 'package:rxdart/rxdart.dart';
+import '../models/item_model.dart';
+import '../resources/repository.dart';
+import 'dart:async';
+
+class StoriesBloc {
+  final _repository = Repository();
+  final _topIds = PublishSubject<List<int>>();
+
+  // Getters to Strems
+  Observable<List<int>> get topIds => _topIds.stream;
+
+  fetchTopIds() async {
+    final ids = await _repository.fetchTopIds();
+    _topIds.sink.add(ids);
+  }
+
+  _itemsTransformer(){
+    return ScanStreamTransformer(
+      (Map<int, Future<ItemModel>> cache, int id, _) {
+        cache[id] = _repository.fetchItem(id);
+        return cache;
+      },
+      <int, Future<ItemModel>>{},
+    );
+  }
+
+  dispose(){
+    _topIds.close();
+  }
+
+}
--- lib/src/screens/news_list.dart
+++ lib/src/screens/news_list.dart
@@ -1,34 +1,37 @@
 import 'package:flutter/material.dart';
-import 'dart:async';
+import '../blocs/stories_provider.dart';
 
 class NewsList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final bloc = StoriesProvider.of(context);
+
+    bloc.fetchTopIds();
     return Scaffold(
       appBar: AppBar(title: Text('Top News')),
-      body: buildList()
+      body: buildList(bloc)
     );
   }
 
-  Widget buildList() {
-    return ListView.builder(
-      itemCount: 1000,
-      itemBuilder: (context, int index) {
-        return FutureBuilder(
-          future: getFuture(),
-          builder: (context, snapshot){
-            return snapshot.hasData ? Text('Im visible $index') : Text('I havent fetched data yet $index');
-          }
+  Widget buildList(StoriesBloc bloc) {
+    return StreamBuilder( 
+      stream: bloc.topIds, 
+      builder: (context, AsyncSnapshot<List<int>>snapshot) {
+        if (!snapshot.hasData) {
+          return Center(
+            child:CircularProgressIndicator()
+          );
+          
+        }
+
+        return ListView.builder(
+          itemCount: snapshot.data.length,
+          itemBuilder: (context, int index){
+            return Text('id - ${snapshot.data[index]}');
+          },
+          
         );
-      }
+      },
     );
   }
-
-  getFuture(){
-    return Future.delayed(
-      Duration(seconds: 2),
-      () => 'hi'
-    );
-  }
-
 }
