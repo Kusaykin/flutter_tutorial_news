8b2c41719f455ca8b1369d5194645a999bbd144f
dboyev <dboyev@nurbank.kz>
2019-06-28 17:21:54 +0600

    28\285

--- lib/src/blocs/stories_bloc.dart
+++ lib/src/blocs/stories_bloc.dart
@@ -6,9 +6,20 @@ import 'dart:async';
 class StoriesBloc {
   final _repository = Repository();
   final _topIds = PublishSubject<List<int>>();
+  final _itemsOutput = BehaviorSubject<Map<int, Future<ItemModel>>>();
+  final _itemsFetcher = PublishSubject<int>();
 
-  // Getters to Strems
+
+  StoriesBloc(){
+    _itemsFetcher.stream.transform(_itemsTransformer()).pipe(_itemsOutput);
+  }
+
+  // Getters to Streams
   Observable<List<int>> get topIds => _topIds.stream;
+  Observable<Map<int, Future<ItemModel>>> get items => _itemsOutput.stream;
+
+  // Getters to Sinks
+  Function(int) get fetchItem => _itemsFetcher.sink.add;
 
   fetchTopIds() async {
     final ids = await _repository.fetchTopIds();
@@ -17,7 +28,8 @@ class StoriesBloc {
 
   _itemsTransformer(){
     return ScanStreamTransformer(
-      (Map<int, Future<ItemModel>> cache, int id, _) {
+      (Map<int, Future<ItemModel>> cache, int id, index) {
+        print(index);
         cache[id] = _repository.fetchItem(id);
         return cache;
       },
@@ -27,6 +39,10 @@ class StoriesBloc {
 
   dispose(){
     _topIds.close();
+    _itemsOutput.close();
+    _itemsFetcher.close();
   }
+
+
 
 }
